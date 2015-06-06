4a7e4753af153e60c69040f65b645d9597faf7a7
Joan Erencia <joan.erencia@live.de>
2015-06-06 03:28:09 +0200

    Basics of MongoDB to establish a connection with a MongoClient, create a DB and Get a collection

---
+++ M101DotNet/M101DotNet/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace M101DotNet
+{
+    class Program
+    {
+        static void Main( string[] args )
+        {
+            MainAsync( args ).Wait();
+            Console.WriteLine( "Press enter!" );
+            Console.ReadLine();
+        }
+
+        static async Task MainAsync( string [] args )
+        {
+
+
+            // *** BASICS *** ////
+
+            // Talking to 2 servers in port 27017 running local
+            var connectionString = "mongodb://localhost:27017";
+            //var settings = new MongoClientSettings
+            //{
+            //}
+
+            var client = new MongoClient( connectionString );
+
+            // GetDatabase() has an override with the MongoClientSettings
+            var db = client.GetDatabase( "test" );
+            // GetCollection() has an override with the MongoClientSettings
+            var col = db.GetCollection<BsonDocument>( "people" );
+
+
+
+        }
+    }
+}
