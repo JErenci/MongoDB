c8ae5454c295f88b779598e7a6e632ea819ac3a3
Joan Erencia <joan.erencia@live.de>
2015-06-06 03:54:55 +0200

    Using BsonArray class to add an array of elements to a BsonDocument

--- M101DotNet/M101DotNet/Program.cs
+++ M101DotNet/M101DotNet/Program.cs
@@ -17,27 +17,47 @@ namespace M101DotNet
             Console.ReadLine();
         }
 
-        static async Task MainAsync( string [] args )
+        static async Task MainAsync( string[] args )
         {
+            //// *** BASICS *** ////
 
+            //// Talking to 2 servers in port 27017 running local
+            //var connectionString = "mongodb://localhost:27017";
+            ////var settings = new MongoClientSettings
+            ////{
+            ////}
 
-            // *** BASICS *** ////
+            //var client = new MongoClient( connectionString );
 
-            // Talking to 2 servers in port 27017 running local
-            var connectionString = "mongodb://localhost:27017";
-            //var settings = new MongoClientSettings
-            //{
-            //}
+            //// GetDatabase() has an override with the MongoClientSettings
+            //var db = client.GetDatabase( "test" );
+            //// GetCollection() has an override with the MongoClientSettings
+            //var col = db.GetCollection<BsonDocument>( "people" );
 
-            var client = new MongoClient( connectionString );
+            // Creation a dictionary of keys,values
+            var doc = new BsonDocument
+            {
+                { "name", "Jones" }
+            };
 
-            // GetDatabase() has an override with the MongoClientSettings
-            var db = client.GetDatabase( "test" );
-            // GetCollection() has an override with the MongoClientSettings
-            var col = db.GetCollection<BsonDocument>( "people" );
+            // Add method, takes a key and a "BsonValue" which maps value into their proper type
+            doc.Add( "age", 30 );
 
+            // Add using indexers
+            doc["profession"] = "hacker";
 
+            // Using BsonArrays
+            var nestedArray = new BsonArray();
+            nestedArray.Add( new BsonDocument( "color", "red" ) );
 
+            // Adding the BsonArray to the BsonDocument
+            doc.Add( "array", nestedArray );
+
+            Console.WriteLine( doc );
+
+            // Accessing the elements in the BsonDocument is also straightforward:
+            Console.WriteLine( doc["array"][0]["color"] );
+            
         }
     }
 }
