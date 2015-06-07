525739fe226107806a5532e3152c767a33cdde5d
Joan Erencia <joan.erencia@live.de>
2015-06-08 00:49:13 +0200

    Mapping POCOs to Bson, commented

--- M101DotNet/M101DotNet/Program.cs
+++ M101DotNet/M101DotNet/Program.cs
@@ -18,11 +18,12 @@ namespace M101DotNet
             Console.ReadLine();
         }
 
-        static async Task MainAsync( string[] args )
-        {
-            // 1- // *** BASICS *** ////
 
+            // 1- // *** BASICS *** ////
+		//	static async Task MainAsync( string[] args )
+		//{
             //// Talking to 2 servers in port 27017 running local
+
             //var connectionString = "mongodb://localhost:27017";
             ////var settings = new MongoClientSettings
             ////{
@@ -35,10 +36,12 @@ namespace M101DotNet
             //// GetCollection() has an override with the MongoClientSettings
             //var col = db.GetCollection<BsonDocument>( "people" );
 
-
+		//}
 
 
             //2 - //  *** Adding key,value pairs to a Bson document *** //
+		//static async Task MainAsync( string[] args )
+		//{
             //// Creation a dictionary of keys,values
             //var doc = new BsonDocument
             //{
@@ -62,87 +65,90 @@ namespace M101DotNet
 
             //// Accessing the elements in the BsonDocument is also straightforward:
             //Console.WriteLine( doc["array"][0]["color"] );
+		//}
 
 
 
-            //3 - //  *** POCO representation (Mapping POCO to Bson ) *** ////
+	//	static async Task MainAsync( string[] args )
+	//	{
+	//		//3 - //  *** POCO representation (Mapping POCO to Bson ) *** ////
 
-			// POCOs attributes can be customized in 3 ways:
+	//		// POCOs attributes can be customized in 3 ways:
 
-            // 3.a) Using Attributes
-            // [BsonElement( "name" )]
-            // [BsonIgnore], [BsonIgnoreExtraElements], [BsonIgnoreDefault],
-            // [BsonRepresentation] (change type of a field, so that matches DB representation)
+	//		// 3.a) Using Attributes
+	//		// [BsonElement( "name" )]
+	//		// [BsonIgnore], [BsonIgnoreExtraElements], [BsonIgnoreDefault],
+	//		// [BsonRepresentation] (change type of a field, so that matches DB representation)
 
-			// 3.b) Using the BsonClassMap and setting elements using ClassMapInitializers
-            // Also, this can be done directly in the code w/ BsonClassMap to register that class
-            // RegisterClassMap() is some kind of global registry
+	//		// 3.b) Using the BsonClassMap and setting elements using ClassMapInitializers
+	//		// Also, this can be done directly in the code w/ BsonClassMap to register that class
+	//		// RegisterClassMap() is some kind of global registry
 
-			// 3.c) Using conventions
-			// Create the ConventionPack class where you add a convention (e.g. camelCase)
-			// Add ConventionPack to the ConventionRegistry
+	//		// 3.c) Using conventions
+	//		// Create the ConventionPack class where you add a convention (e.g. camelCase)
+	//		// Add ConventionPack to the ConventionRegistry
 
 			
-			// 3.c)
-			var conventionPack = new ConventionPack();
-			conventionPack.Add( new CamelCaseElementNameConvention() );
-			// Applies camelCase convention to all variables (t => true)
-			ConventionRegistry.Register( "camelCase", conventionPack, t => true );
+	//		// 3.c)
+	//		var conventionPack = new ConventionPack();
+	//		conventionPack.Add( new CamelCaseElementNameConvention() );
+	//		// Applies camelCase convention to all variables (t => true)
+	//		ConventionRegistry.Register( "camelCase", conventionPack, t => true );
 
 
-			//// 3.b)
-			//BsonClassMap.RegisterClassMap<Person>( cm =>
-			//{
-			//	cm.AutoMap();
-			//	// 3.b)
-			//	cm.MapMember( x => x.Name ).SetElementName( "name" );
-			//} );
+	//		// 3.b)
+	//		BsonClassMap.RegisterClassMap<Person>( cm =>
+	//		{
+	//			cm.AutoMap();
+	//			// 3.b)
+	//			cm.MapMember( x => x.Name ).SetElementName( "name" );
+	//		} );
 
 
-            var person = new Person
-            {
-                // Id is not initialized, the it is set by default by MongoDB
-                Name = "Jones",
-                Age = 30,
-                Colors = new List<string> { "red", "blue" },
-                Pets = new List<Pet> { new Pet { Name = "Fluffy", Type = "Pig" } },
-                ExtraElements = new BsonDocument( "anotherName", "anotherValue")
-            } ;
+	//		var person = new Person
+	//		{
+	//			// Id is not initialized, the it is set by default by MongoDB
+	//			Name = "Jones",
+	//			Age = 30,
+	//			Colors = new List<string> { "red", "blue" },
+	//			Pets = new List<Pet> { new Pet { Name = "Fluffy", Type = "Pig" } },
+	//			ExtraElements = new BsonDocument( "anotherName", "anotherValue" )
+	//		};
 
-            // This happens in MongoDB under the covers, but we explicitly write it so
-            // it can be observed in the console
-            using ( var writer = new JsonWriter( Console.Out ) )
-            {
-                BsonSerializer.Serialize( writer, person );
-            }
+	//		// This happens in MongoDB under the covers, but we explicitly write it so
+	//		// it can be observed in the console
+	//		using ( var writer = new JsonWriter( Console.Out ) )
+	//		{
+	//			BsonSerializer.Serialize( writer, person );
+	//		}
 
-        }
+	//	}
 
-        class Person
-        {
-            public ObjectId Id { get; set; }
+	//	class Person
+	//	{
+	//		public ObjectId Id { get; set; }
 
-			// 3.a)
-			//[BsonElement("name")]   
-            public string Name { get; set; }
+	//		// 3.a)
+	//		//[BsonElement("name")]   
+	//		public string Name { get; set; }
 
-			// 3.a)
-			//[BsonRepresentation (BsonType.String)]
-            public int Age { get; set; }
-            
-            public List<string> Colors { get; set; }
+	//		// 3.a)
+	//		//[BsonRepresentation (BsonType.String)]
+	//		public int Age { get; set; }
 
-            public List<Pet> Pets { get; set; }
+	//		public List<string> Colors { get; set; }
 
-            public BsonDocument ExtraElements { get; set; }
-        }
+	//		public List<Pet> Pets { get; set; }
 
-        public class Pet
-        {
-            public string Name  { get; set; }
+	//		public BsonDocument ExtraElements { get; set; }
+	//	}
 
-            public string Type  { get; set; }
-        }
-    }
+	//	public class Pet
+	//	{
+	//		public string Name { get; set; }
+
+	//		public string Type { get; set; }
+	//	}
+	//}
 
 }
